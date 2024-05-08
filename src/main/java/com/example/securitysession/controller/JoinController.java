ee2baa0ade55bc5cc18586f97d0440dcc3aa6e25
이현주 <lhjivy1024@gmail.com>
2024-05-08 21:30:33 +0900

    db연결 및 회원가입 로직 설계

---
+++ src/main/java/com/example/securitysession/controller/JoinController.java
@@ -0,0 +1,29 @@
+package com.example.securitysession.controller;
+
+import com.example.securitysession.dto.JoinDto;
+import com.example.securitysession.service.JoinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class JoinController {
+
+    @Autowired
+    private JoinService joinService;
+
+    @GetMapping("/join")
+    public String joinP() {
+        return "join";
+    }
+
+    @PostMapping("/joinProc")
+    public String joinProcess(JoinDto joinDto) {
+        System.out.println(joinDto.getUsername());
+
+        joinService.joinProcess(joinDto);
+
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/com/example/securitysession/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.securitysession.repository;
+
+import com.example.securitysession.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
